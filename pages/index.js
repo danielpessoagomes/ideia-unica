f5b6148a8c08980c390c55506f0fdd3c0f5210a7
Daniel Gomes <danielgomes@dginfope.com.br>
2021-01-15 10:34:34 -0300

    implementation variation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,59 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 function Home() {
+
+    const [valorInicial, setValorInicial] = useState(0);
+    const [valorFinal, setValorFinal] = useState(0);
+    const [valorVariacao, setValorVariacao] = useState(0);
+
+    function variacao() {
+        if(valorInicial && valorFinal) {
+            setValorVariacao((valorFinal / valorInicial - 1) * 100)
+        }
+    }
+
     return (
         <div>
-            <h1>Home</h1>
-            <Link href="/sobre">
-                <a>Acessar página sobre</a>
-            </Link>
+            <h1>Calcular Variação</h1>
+            <div>
+                <label>
+                    Valor Inicial
+            </label>
+                <input
+                    placeholder="Valor Inicial"
+                    type="text"
+                    name="valorInicial"
+                    value={valorInicial}
+                    onChange={(event) => {
+                        setValorInicial(event.target.value)
+                    }}
+                    onBlur={variacao}
+                />
+            </div>
+            <div>
+                <label>Valor Final</label>
+                <input
+                    placeholder="Valor Final"
+                    type="text"
+                    name="valorFinal"
+                    value={valorFinal}
+                    onChange={(event) => {
+                        setValorFinal(event.target.value)
+                    }}
+                    onBlur={variacao}
+                />
+            </div>
+            <div>
+            <label>Variação em %</label>
+            <input
+                placeholder="Variacao"
+                type="text"
+                name="valorVariacao"
+                value={`${valorVariacao.toPrecision(4)}%`}
+                disabled
+            />
+            </div>
         </div>
     )
 }
